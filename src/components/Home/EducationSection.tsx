25bee6868e4fe6ecf52fa95b53944ffd6a3a2158
Gustavo Silva <gustavo.silva.nav@gmail.com>
2025-07-27 21:54:25 -0400

    🎨 Adding soem really nice effect to it

--- src/components/Home/EducationSection.tsx
+++ src/components/Home/EducationSection.tsx
@@ -1,5 +1,6 @@
 import { PersonalInfo } from '@/interfaces';
 import { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from '@/components/ui/card';
+import { MotionHighlight } from '@/components/animate-ui/effects/motion-highlight';
 
 type Props = {
   personalInfo: PersonalInfo;
@@ -12,23 +13,23 @@ export const EducationSection = ({ personalInfo }: Props) => {
       className="mb-20 md:mb-32 p-8 bg-white dark:bg-gray-900 rounded-xl shadow-lg dark:shadow-xl border border-gray-200 dark:border-gray-800 animate-fade-in-large delay-600">
       <h2 className="text-3xl md:text-4xl font-bold text-gray-900 dark:text-white mb-12 text-center">Education</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-        {personalInfo.education.map((edu, index) => (
-          <Card
-            key={index}
-            className="flex flex-col h-full hover:border-blue-600 dark:hover:border-blue-500 transition-all duration-300">
-            <CardHeader>
-              <CardTitle className="text-xl font-semibold text-gray-900 dark:text-white">{edu.degree}</CardTitle>
-              <CardDescription className="text-blue-600 dark:text-blue-300">{edu.institution}</CardDescription>
-            </CardHeader>
+        <MotionHighlight hover className="rounded-xl z-2 bg-gray-500/10">
+          {personalInfo.education.map((edu, index) => (
+            <Card key={index} className="flex flex-col h-full">
+              <CardHeader>
+                <CardTitle className="text-xl font-semibold text-gray-900 dark:text-white">{edu.degree}</CardTitle>
+                <CardDescription className="text-blue-600 dark:text-blue-300">{edu.institution}</CardDescription>
+              </CardHeader>
 
-            <CardContent className="flex-grow"></CardContent>
+              <CardContent className="flex-grow"></CardContent>
 
-            <CardFooter className="text-gray-600 dark:text-gray-400 text-sm flex justify-between items-center">
-              <span>{edu.dates}</span>
-              <span>{edu.location}</span>
-            </CardFooter>
-          </Card>
-        ))}
+              <CardFooter className="text-gray-600 dark:text-gray-400 text-sm flex justify-between items-center">
+                <span>{edu.dates}</span>
+                <span>{edu.location}</span>
+              </CardFooter>
+            </Card>
+          ))}
+        </MotionHighlight>
       </div>
     </section>
   );
