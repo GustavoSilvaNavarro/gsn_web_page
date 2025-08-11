e95b2b3ed2e5ffb2f55480def367e0c25c12e8fd
Gustavo Silva <gustavo.silva.nav@gmail.com>
2025-08-11 20:50:16 -0300

    💄 Making improvemtns in the UI
    by adding a tooltip so you can read the description

--- src/components/ui/skills/SkillIcon.tsx
+++ src/components/ui/skills/SkillIcon.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
-import { Skill } from '@/interfaces'; // Import the Skill interface
+import { Skill } from '@/interfaces';
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip';
 
 type Props = {
   skill: Skill;
@@ -7,8 +8,23 @@ type Props = {
 
 export const SkillIcon = ({ skill }: Props) => {
   return (
-    <div className="flex flex-col items-center justify-center p-2 bg-gray-100 dark:bg-gray-200 rounded-md shadow-sm dark:shadow-md">
-      <Image className="skill-icon w-10 h-10 object-contain" src={skill.icon} alt={skill.name} width="40" height="40" />
-    </div>
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <div className="flex flex-col items-center justify-center p-2 bg-gray-100 dark:bg-gray-200 rounded-md shadow-sm dark:shadow-md cursor-pointer">
+            <Image
+              className="skill-icon w-10 h-10 object-contain"
+              src={skill.icon}
+              alt={skill.name}
+              width="40"
+              height="40"
+            />
+          </div>
+        </TooltipTrigger>
+        <TooltipContent className="bg-gray-800 text-white dark:bg-blue-600 dark:text-white p-2 rounded-md shadow-lg">
+          <p>{skill.name}</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
   );
 };
